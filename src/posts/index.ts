77ff5294d47bc2ac92bcf12edb125be5a6f03aa6
ja153903 <jaimeabbariao@gmail.com>
2022-06-07 18:05:22 -0400

    fix: formatting

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,1 +1,5 @@
 export { router as postRouter } from "./router";
+export { default as PostService } from "./service";
+export { createPostSchemaValidation } from "./validation";
+
+export type { CreatePost } from "./types";
