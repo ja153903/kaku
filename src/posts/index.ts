37a2e8dcb976f7dea20b6e11a99a0148e925d7a3
ja153903 <jaimeabbariao@gmail.com>
2022-07-01 14:54:07 -0400

    feat: updating exports

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,5 +1,5 @@
 export { router as postRouter } from "./router";
-export { default as PostService } from "./service";
+export { createPost, updatePost, getPostById } from "./service";
 export { createPostSchemaValidation } from "./validation";
 
 export type { CreatePost } from "./types";
